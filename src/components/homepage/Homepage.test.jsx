5e8071c4e68baa8fc2ef8a9240c3b4130d057fa7
SuspiciousApplePie <andreiusita6@gmail.com>
2026-08-28 11:28:08 +0800

    Change test of homepage

--- src/components/homepage/Homepage.test.jsx
+++ src/components/homepage/Homepage.test.jsx
@@ -1,23 +1,11 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe } from "vitest";
 import { screen, render } from "@testing-library/react";
 import Homepage from "./Homepage";
-import { MemoryRouter, useOutletContext } from "react-router";
-
-vi.mock("react-router", async () => {
-  const actual = await vi.importActual("react-router");
-
-  return {
-    ...actual,
-    useOutletContext: vi.fn(),
-  };
-});
+import { MemoryRouter } from "react-router";
 
 describe("Homepage component", () => {
   it("Title appears", () => {
     const header = "Welcome to the Shopping Cart";
-    useOutletContext.mockReturnValue({
-      products: [],
-    });
     render(
       <MemoryRouter>
         <Homepage />
@@ -26,30 +14,14 @@ describe("Homepage component", () => {
     expect(screen.getByRole("heading", { name: header })).toBeInTheDocument();
   });
 
-  it("Product photo appears", () => {
-    const resolvedObject = {
-      id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-      rating: {
-        rate: 3.9,
-        count: 120,
-      },
-    };
-    useOutletContext.mockReturnValue({
-      products: [resolvedObject],
-    });
+  it("About section appears", () => {
     render(
       <MemoryRouter>
         <Homepage />
       </MemoryRouter>,
     );
     expect(
-      screen.getByAltText(`${resolvedObject.title} image`),
+      screen.getByRole("heading", { name: "About Us" }),
     ).toBeInTheDocument();
   });
 });
