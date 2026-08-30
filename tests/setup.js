0474ca989fa3c61e434b77e8475c8cbe330a560f
SuspiciousApplePie <andreiusita6@gmail.com>
2026-08-30 19:29:03 +0800

    Update test and added ResizeObserver for reference

--- tests/setup.js
+++ tests/setup.js
@@ -3,7 +3,11 @@ import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
-
+globalThis.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
 afterEach(() => {
   cleanup();
 });
